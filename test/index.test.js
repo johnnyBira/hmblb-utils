d3b9fb28db5a88ead404619bb0ca97f29b0bf09c
John Persson <john.persson@humblebee.se>
2017-07-24 18:38:18 +0200

    fix(array): Remove exported function
    Remove unfinished function from lib export

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,5 @@
 import index from 'index';
 
 test('Should export utility functions', () => {
-  expect(JSON.stringify(index)).toEqual(JSON.stringify({
-    array: {},
-  }));
+  expect(JSON.stringify(index)).toEqual(JSON.stringify({}));
 });
